9fa00183aa1c81783e98cc28265f0002255bde18
Ian <ignacioiannello7@gmail.com>
2023-04-30 01:34:40 -0300

    PARTLY DONE EJ 4 + 5 GUIA 5

--- guia5.hs
+++ guia5.hs
@@ -44,11 +44,106 @@ todosIguales (x:xs)
                   | otherwise = todosIguales tempVar 
                   where tempVar = (tail xs)
 
+-- EJERCICIO 2 c -- 
+--TODO--
 
-todosDistintos::(Eq t) => [t] -> Bool
-todosDistintos (x:xs) | (todosDistintosAUX (x:xs) head xs length xs)
+-- EJERCICIO 3--
+--TODO--
 
-todosDistintosAUX (Eq t) => [t] -> t -> i -> Bool
-todosDistintosAUX (x:xs) y i 
-                        | (i==0) || (i==1) 
+--EJERCICIO 4 a--
 
+sacarBlancosRepetidos :: [Char] -> [Char]
+sacarBlancosRepetidos [] = []
+sacarBlancosRepetidos (x:xs) | (length (x:xs) >1) && (x==' ' && x == (head xs))= sacarBlancosRepetidos xs
+                             | otherwise = [x] ++ sacarBlancosRepetidos xs
+
+--EJERCICIO 4 b -- 
+-- ASUMO QUE NO HAY PRIMEROS Y ULTIMOS ESPACIOS EN LAS PALABRAS, EVENTUALMENTE HARE UNA FUNCION PARA ESO--
+contarPalabras :: [Char] -> Integer
+contarPalabras[] = 0
+contarPalabras (x:xs)
+                    | not(hayEspacios (x:xs)) && lgth >=1 =1
+                    | otherwise= (contarPalabrasAUX (sacarBlancosRepetidos (x:xs)))
+                    where lgth = length (x:xs)
+
+hayEspacios::[Char]->Bool
+hayEspacios [] =False
+hayEspacios (x:xs) | length (x:xs) ==1 = (x==' ') 
+                      | x==' ' =True
+                      | x/=' ' =hayEspacios xs
+
+
+contarPalabrasAUX::[Char]->Integer
+contarPalabrasAUX [] =1
+contarPalabrasAUX (x:xs) 
+                         | (x==' ') =1+contarPalabrasAUX xs
+                         | otherwise =0+contarPalabrasAUX xs 
+
+--EJERCICIO 4 c --
+--CREAR FUNCION INDICE ESPACIOS DONDE INCLUYE COMIENZO Y FIN DE LA SECUENCIA--
+--GENERAR UN ALGORITMO QUE ENCUENTRE LA DIFERENCIA MAS GRANDE ENTRE DOS INDICES INICIO-FIN"
+
+palabraMasLarga :: [Char] -> [Char]
+palabraMasLarga (x:xs) =  buscarPalabraAt (x:xs) (diferenciaMasGrande (encontrarEspacios (x:xs) (length(x:xs))) (0,0))
+
+encontrarEspacios::[Char]->Int->[Int]
+encontrarEspacios [] _ = []
+encontrarEspacios (x:xs) tope 
+                        | length (x:xs) ==tope = [tope] ++ encontrarEspacios (tail xs) tope
+                        | length (x:xs) > 1 && x==' ' = [length (x:xs)] ++ encontrarEspacios (tail xs) tope
+                        | length (x:xs) > 1 && x/=' ' =  encontrarEspacios (tail xs) tope
+                        | length (x:xs) == 1 =[1]
+
+diferenciaMasGrande::[Int]->(Int,Int)->(Int,Int)
+diferenciaMasGrande [] (a,b) =(a,b)
+diferenciaMasGrande (x:xs) (a,b) 
+                                | (length (x:xs) == 1) =(a,b)
+                                | (length (x:xs) == 2) && (x-head xs)>(b-a)  =(head xs,x)
+                                | (length (x:xs) == 2) && (x-head xs)<=(b-a)  =(b,a)
+                                | (x - head xs)> (b-a) = diferenciaMasGrande (tail xs) (head xs,x)
+                                | (x - head xs)<= (b-a) = diferenciaMasGrande (tail xs) (a,b)
+
+buscarPalabraAt::[Char]->(Int,Int)->[Char]
+buscarPalabraAt [] _ =[]
+buscarPalabraAt (x:xs) (inicio,tope) | length (x:xs) > inicio = [] ++ buscarPalabraAt xs (inicio,tope)
+                                     | length (x:xs) <= inicio && length(x:xs) > tope = [x] ++ buscarPalabraAt xs (inicio,tope)
+                                     | length (x:xs) == tope =[x]
+
+
+-- EJERCICIO 5 a-- 
+
+nat2bin :: Integer -> [Integer]
+nat2bin a | d == 1 = [d]++[m] 
+          | d /=1 =  nat2bin d ++ [m] 
+          where m= (mod a 2)::Integer
+                d= (div a 2)::Integer
+
+
+bin2nat::[Integer]->Integer
+bin2nat (x:xs) | length (x:xs) ==1= x*2^(length (x:xs) + (-1))
+               | length (x:xs) >1 = x*2^(length (x:xs) + (-1)) + bin2nat xs     
+
+nat2hex :: Integer -> [Char]
+nat2hex a | d==0 = [hex2letter m]
+          | d/=0 = nat2hex d ++ [hex2letter m] 
+               where m=(mod a 16)::Integer
+                     d=(div a 16)::Integer
+
+hex2letter::Integer->Char
+hex2letter x
+                 | x==0  = '0'
+                 | x==1  = '1'
+                 | x==2  = '2'
+                 | x==3  = '3'
+                 | x==4  = '4'
+                 | x==5  = '5'
+                 | x==6  = '6'     
+                 | x==7  = '7'
+                 | x==8  = '8'
+                 | x==9  = '9'
+                 | x==10 = 'A'
+                 | x==11 = 'B'
+                 | x==12 = 'C'
+                 | x==13 = 'D'
+                 | x==14 = 'E'
+                 | x==15 = 'F'
